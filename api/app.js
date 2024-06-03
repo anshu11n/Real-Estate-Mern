165f569a9d618b18d1eb72749b4cd03c5c63b7b7
Anshu <95614796+anshu11n@users.noreply.github.com>
2024-06-03 16:12:53 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -10,7 +10,8 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.VITE_CLIENT_URL, credentials: true }));
+app.use(cors({ origin: [process.env.VITE_CLIENT_URL , 'http://localhost:5173'], credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 
